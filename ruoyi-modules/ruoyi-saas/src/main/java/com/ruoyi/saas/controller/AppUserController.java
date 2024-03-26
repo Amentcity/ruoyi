b2ccd2b84ab66a7c80934ff3b38303c60140a3c4
殷成智 <1954869697@qq.com>
2024-03-27 00:00:57 +0800

    新增接口判断

--- ruoyi-modules/ruoyi-saas/src/main/java/com/ruoyi/saas/controller/AppUserController.java
+++ ruoyi-modules/ruoyi-saas/src/main/java/com/ruoyi/saas/controller/AppUserController.java
@@ -284,7 +284,7 @@ public class AppUserController extends BaseController
         //需要先查询tag标签是否存在
         //如果集合为空，添加断言数据不能为空
         Assert.isNull(id,"电站id不能为空");
-        Assert.isNull(tag,"电站标签");
+        Assert.isNull(tag,"电站标签不能为空");
         //将标签添加到appUser表中，完成为用户设置标签问题
         appUserService.update(Wrappers.lambdaUpdate(AppUser.class).set(AppUser::getPhone,tag).eq(AppUser::getId,id));
         return success("用户标签设置成功");
